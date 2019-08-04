b8ad5f4a3a293936f4e6fa250ec835410b0c86d7
Joachim Metz <joachim.metz@gmail.com>
2019-08-04 19:37:26 +0200

    Applied updates

---
+++ .travis/script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Script to run script step on Travis-CI
+#
+# Version: 20190802
+
+# Exit on error.
+set -e;
+
+if test ${TRAVIS_OS_NAME} = "linux" || test ${TRAVIS_OS_NAME} = "linux-ppc64le";
+then
+	export PATH=$(echo $PATH | tr ":" "\n" | sed '/\/opt\/python/d' | tr "\n" ":" | sed "s/::/:/g");
+fi
+
+if test ${TARGET} = "docker";
+then
+	CONTAINER_NAME="testcontainer";
+	CONTAINER_OPTIONS="-e LANG=en_US.UTF-8";
+
+	# Note that exec options need to be defined before the container name.
+	docker exec ${CONTAINER_OPTIONS} ${CONTAINER_NAME} sh -c "cd libcdata && .travis/runtests.sh";
+
+elif test ${TARGET} != "coverity";
+then
+	.travis/runtests.sh;
+
+	if test ${TARGET} = "macos-gcc-pkgbuild";
+	then
+		export VERSION=`sed '5!d; s/^ \[//;s/\],$//' configure.ac`;
+
+		make install DESTDIR=${PWD}/osx-pkg;
+		mkdir -p ${PWD}/osx-pkg/usr/share/doc/libcdata;
+		cp AUTHORS COPYING NEWS README ${PWD}/osx-pkg/usr/share/doc/libcdata;
+
+		pkgbuild --root osx-pkg --identifier com.github.libyal.libcdata --version ${VERSION} --ownership recommended ../libcdata-${VERSION}.pkg;
+	fi
+fi
+
