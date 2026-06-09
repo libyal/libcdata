553ef8c0b8606fef9bb7c88e48b5be372719eb0f
Joachim Metz <joachim.metz@gmail.com>
2026-06-09 19:51:12 +0200

    Replaced test scripts with autotest

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20201121
+# Version: 20260609
 
-if test -f ${PWD}/libcdata/.libs/libcdata.1.dylib && test -f ./pycdata/.libs/pycdata.so;
+if test -f ${PWD}/libcerror/.libs/libcerror.1.dylib && test -f ./pycerror/.libs/pycerror.so
 then
-	install_name_tool -change /usr/local/lib/libcdata.1.dylib ${PWD}/libcdata/.libs/libcdata.1.dylib ./pycdata/.libs/pycdata.so;
+	install_name_tool -change /usr/local/lib/libcerror.1.dylib ${PWD}/libcerror/.libs/libcerror.1.dylib ./pycerror/.libs/pycerror.so
 fi
 
-make check CHECK_WITH_STDERR=1;
-RESULT=$?;
+make check-build > /dev/null
 
-if test ${RESULT} -ne 0 && test -f tests/test-suite.log;
+make check $@
+RESULT=$?
+
+if test ${RESULT} -ne 0
 then
-	cat tests/test-suite.log;
+	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
-exit ${RESULT};
+exit ${RESULT}
 
